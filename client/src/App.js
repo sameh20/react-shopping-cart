7af79a08188420900e487d9ae6f9f64c49d357f2
unknown <samehaboelmagd49@gmail.com>
2024-07-18 05:27:22 +0200

    start react new project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello react 
+      </header>
+    </div>
+  );
+}
+
+export default App;
