0e41b49694f31b777d767a1f17d8ae8acc7b3ca3
unknown <samehaboelmagd49@gmail.com>
2024-07-19 02:39:50 +0200

    footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
 import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="layout">
     <Header/>
       <main>Content</main>
-      <footer>Footer</footer>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../css/Footer/Footer.css"
+
+function Footer ()  {
+  return (
+    <footer>
+      &copy; Copyright Sameh Abo Elmagd
+    </footer>
+  )
+}
+
+export default Footer
+
