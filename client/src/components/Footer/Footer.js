fa1b744097fdc13a377435fef1ed5a7513f18b5b
unknown <samehaboelmagd49@gmail.com>
2024-07-20 03:14:04 +0200

    product component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "../../css/Footer/Footer.css"
+import { words } from "../../words";
+
 
 function Footer ()  {
   return (
     <footer>
-      &copy; Copyright Sameh Abo Elmagd
+      {words.footerTitle}
     </footer>
   )
 }
