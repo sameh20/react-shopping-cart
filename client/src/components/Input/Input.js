3ed337079ab604e47e5efa1583687a06990161f9
unknown <samehaboelmagd49@gmail.com>
2024-07-24 04:02:33 +0200

    check out component

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ function Input(props) {
+  return (
+    <div>
+      <label>{props.label}</label>
+      <input  type={props.type}
+        name={props.name} 
+        onChange={props.onChange}
+        required />
+    </div>
+    
+  )
+}
+
+export default Input
