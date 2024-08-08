ab4a0cf7c50a793c2a43f3e4de462bd77b03d0de
Sameh Abo Elmagd <64335742+sameh20@users.noreply.github.com>
2024-08-08 05:05:09 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express()
 const mongoose = require("mongoose");
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static("public", {
+    index: false
+}))
 
 
 mongoose.connect('mongodb://0.0.0.0/testdb')
